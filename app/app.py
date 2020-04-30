6feb5233a2ea662fccfeb24d775c293c0fe79fee
Pilgrim001 <saeedbello12@gmail.com>
2020-04-30 13:26:41 +0100

    changes

--- app/app.py
+++ app/app.py
@@ -83,6 +83,6 @@ def scrapencdc():
     
     
 
-    return jsonify({"data":{"NCDC_National_Info":data_res_national,"lock_down_start":date(2020,4,26)"KD_state":data_res_state,"Hotline":["08035871662","08025088304","08032401473","08037808191"],"Date":date.today(),"item":json.loads(json_util.dumps(statt[::-1]))}})
+    return jsonify({"data":{"NCDC_National_Info":data_res_national,"lock_down_start":str(date(2020,4,26))"KD_state":data_res_state,"Hotline":["08035871662","08025088304","08032401473","08037808191"],"Date":date.today(),"item":json.loads(json_util.dumps(statt[::-1]))}})
 
 
