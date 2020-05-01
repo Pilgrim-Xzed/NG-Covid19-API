98552a96bf1b3d75e981d0969676da0e4c73b26b
Pilgrim001 <saeedbello12@gmail.com>
2020-05-01 14:26:26 +0100

    changes

--- app/app.py
+++ app/app.py
@@ -34,23 +34,31 @@ def scrapencdc():
     req = Request(url=ncdc_url, headers=headers) 
     page = urlopen(req).read() 
     soup = BeautifulSoup(page, 'html.parser')
-    ncdc_national_data = soup.find('table',{'id':'custom1'})
-    ncdc_state_data = soup.find('table',{'id':'custom3'})
+    confirmed_cases = soup.find('div',{'class':'card bg-c-blue order-card'})
+    samples_tested = soup.find('div',{'class':'card newcol order-card'})
+    active_cases = soup.find('div',{'class':'card bg-c-yellow order-card'})
+    discharged_cases = soup.find('div',{'class':'card bg-c-green order-card'})
+    deaths = soup.find('div',{'class':'card bg-c-red order-card'})
+    ncdc_state_data = soup.find('table',{'id':'custom1'})
     statt = []
     data_res_national = {}
     data_res_national['date'] = str(date.today())
-    data_res_state = {}
+    data_res_state = []
     
     
 
-    for tr in ncdc_national_data.find_all('tr'):
-        tds =tr.find_all('td')
+   
         
-        data_res_national[tds[0].text.strip()] =re.sub("\n|>", " ", tds[1].text.strip())
-        if db.stats.find({"date":str(date.today())}).count() > 0:
-            db.stats.update_one({ "date": str(date.today()) },{"$set":json.loads(json_util.dumps(data_res_national))})
-        else:
-            db.stats.insert_one(json.loads(json_util.dumps(data_res_national)))
+    data_res_national[re.sub("\n|>", " ",  confirmed_cases.find('h6').text)] = re.sub("\n|>", " ",  confirmed_cases.find('h2').text.strip())
+    data_res_national[re.sub("\n|>", " ",  samples_tested.find('h6').text)] =re.sub("\n|>", " ",  samples_tested.find('h2').text.strip())
+    data_res_national[re.sub("\n|>", " ",  active_cases.find('h6').text)] =re.sub("\n|>", " ",  active_cases.find('h2').text.strip())
+    data_res_national[re.sub("\n|>", " ",  discharged_cases.find('h6').text)] =re.sub("\n|>", " ",  discharged_cases.find('h2').text.strip())
+    data_res_national[re.sub("\n|>", " ",  deaths.find('h6').text)] =re.sub("\n|>", " ", deaths.find('h2').text.strip())
+    
+    if db.stats.find({"date":str(date.today())}).count() > 0:
+        db.stats.update_one({ "date": str(date.today()) },{"$set":json.loads(json_util.dumps(data_res_national))})
+    else:
+        db.stats.insert_one(json.loads(json_util.dumps(data_res_national)))
     
    
            
@@ -64,24 +72,21 @@ def scrapencdc():
             
 
         
-       
-
-        
     for tr in ncdc_state_data.find_all('tr'):
         tds = tr.find_all('td')
 
         if not tr.find_all('th'):
-            if(tds[0].find('p').string == "Kaduna" ):
-                data_res_state ={
-                    "state_name":tds[0].find('p').string,
-                    "no_of_cases":tds[1].find('p').string,
-                    "no_of_active_cases":tds[2].find('p').string,
-                    "no_of_discharged":tds[3].find('p').string,
-                    "no_of_deaths":tds[4].find('p').string
-                }
+           
+                data_res_state .append({
+                    "state_name":re.sub("\n|>", " ",  tds[0].string).strip(),
+                    "no_of_cases":re.sub("\n|>", " ",  tds[1].string).strip(),
+                    "no_of_active_cases":re.sub("\n|>", " ",  tds[2].string).strip(),
+                    "no_of_discharged":re.sub("\n|>", " ",  tds[3].string).strip(),
+                    "no_of_deaths":re.sub("\n|>", " ",  tds[4].string).strip(),
+                })
             
     
-    
+      
 
     return jsonify({"data":{"NCDC_National_Info":data_res_national,"i_by":30,"lock_down_start":str(date(2020,4,26)),"KD_state":data_res_state,"Hotline":["08035871662","08025088304","08032401473","08037808191"],"Date":date.today(),"item":json.loads(json_util.dumps(statt[::-1]))}})
 
