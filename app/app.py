1c5fe6c8654b3c007fb7802562b0ef5fd3484900
Pilgrim001 <saeedbello12@gmail.com>
2020-05-02 15:26:14 +0100

    changes

--- app/app.py
+++ app/app.py
@@ -49,10 +49,10 @@ def scrapencdc():
 
    
         
-    data_res_national[re.sub("\n|>", " ",  confirmed_cases.find('h6').text)] = re.sub("\n|>", " ",  confirmed_cases.find('h2').text.strip())
-    data_res_national[re.sub("\n|>", " ",  samples_tested.find('h6').text)] =re.sub("\n|>", " ",  samples_tested.find('h2').text.strip())
-    data_res_national[re.sub("\n|>", " ",  active_cases.find('h6').text)] =re.sub("\n|>", " ",  active_cases.find('h2').text.strip())
-    data_res_national[re.sub("\n|>", " ",  discharged_cases.find('h6').text)] =re.sub("\n|>", " ",  discharged_cases.find('h2').text.strip())
+    data_res_national[re.sub("\n|>", " ",  confirmed_cases.find('h6').text)] = re.sub("\n|>|,", " ",  confirmed_cases.find('h2').text.strip())
+    data_res_national[re.sub("\n|>", " ",  samples_tested.find('h6').text)] =re.sub("\n|>|,", " ",  samples_tested.find('h2').text.strip())
+    data_res_national[re.sub("\n|>", " ",  active_cases.find('h6').text)] =re.sub("\n|>|,", " ",  active_cases.find('h2').text.strip())
+    data_res_national[re.sub("\n|>", " ",  discharged_cases.find('h6').text)] =re.sub("\n|>|,", " ",  discharged_cases.find('h2').text.strip())
     data_res_national[re.sub("\n|>", " ",  deaths.find('h6').text)] =re.sub("\n|>", " ", deaths.find('h2').text.strip())
     
     if db.stats.find({"date":str(date.today())}).count() > 0:
